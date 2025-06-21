841e1bfcc084bdfa8cccddfbbe1f9b336bd7814f
MarcXX33 <166284860+MarcXX33@users.noreply.github.com>
2025-06-21 18:52:34 +0200

    Update app.js

--- apps/apps/origoclock/app.js
+++ apps/apps/origoclock/app.js
@@ -1,1 +1,13 @@
 E.showMessage("ORIGO Clock\nAtemübung", "ORIGO");
+,
+{
+  "id": "origoclock",
+  "name": "ORIGO Clock",
+  "short": "Mindfulness Clock",
+  "icon": "origoclock/icon.png",
+  "version": "0.01",
+  "description": "Custom mindfulness clock with pulse-triggered breathing, 4-7-8 breath and meditation.",
+  "type": "app",
+  "sortorder": 99,
+  "src": "origoclock/app.js"
+}
