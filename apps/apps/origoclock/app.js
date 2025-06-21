34fb0bed2297934df43fed5010a4d3154949eb29
MarcXX33 <166284860+MarcXX33@users.noreply.github.com>
2025-06-21 18:47:26 +0200

    Update app.js

--- apps/apps/origoclock/app.js
+++ apps/apps/origoclock/app.js
@@ -1,14 +1,1 @@
-{
-  "id": "origoclock",
-  "name": "ORIGO Clock",
-  "type": "app",
-  "icon": "icon.png",
-  "version": "0.01",
-  "description": "Mindfulness clock with HRM features",
-  "tags": "breathing,clock,tool",
-  "supports": ["BANGLEJS2"],
-  "storage": [
-    {"name":"origoclock.app.js","url":"app.js"},
-    {"name":"origoclock.img","url":"icon.png"}
-  ]
-}
+E.showMessage("ORIGO Clock\nAtemübung", "ORIGO");
