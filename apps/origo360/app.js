11df0ca1efcd8ef3a98c9180bb72f8b0201f6b04
MarcXX33 <166284860+MarcXX33@users.noreply.github.com>
2025-06-27 12:01:33 +0200

    Create app.js

---
+++ apps/origo360/app.js
@@ -0,0 +1,67 @@
+
+// Sensation Clock - Clean Fork from Pebble++ with customizable widgets
+// Widgets: Box Breathing + 4-7-8 + Puls Trigger READY (Einbindung folgt)
+
+const SETTINGS_FILE = "sensation.json";
+let settings = Object.assign({'theme':'System','showdate':true,'clkinfoborder':true}, require("Storage").readJSON(SETTINGS_FILE,1)||{});
+
+let background = require("clockbg");
+let theme;
+let drawTimeout;
+
+const h = g.getHeight();
+const w = g.getWidth();
+const h2 = Math.round(3*h/5) - 10;
+const h3 = Math.round(7*h/8);
+
+function draw() {
+  let locale = require("locale");
+  let date = new Date();
+  let time = locale.time(date, 1);
+
+  g.reset();
+  g.setBgColor(theme.bg).clearRect(0, h2, w, h3);
+  if (settings.showdate) {
+    g.setColor(theme.fg).fillRect(w / 2 - 30, h3 + 5, w / 2 + 30, h);
+    g.setFont("6x8",2).setFontAlign(0, -1);
+    g.setColor(theme.bg).drawString(date.getDate() + "." + (date.getMonth() + 1), w / 2, h3 + 5);
+  }
+  g.setFont("6x8",4).setFontAlign(0, -1);
+  g.setColor(theme.fg);
+  g.drawString(time, w/2, h2 + 8);
+
+  if (drawTimeout) clearTimeout(drawTimeout);
+  drawTimeout = setTimeout(function() {
+    drawTimeout = undefined;
+    draw();
+  }, 60000 - (Date.now() % 60000));
+}
+
+function loadThemeColors() {
+  theme = {fg: g.theme.fg, bg: g.theme.bg};
+  if (settings.theme === "Dark") {
+    theme.fg = g.toColor(1,1,1);
+    theme.bg = g.toColor(0,0,0);
+  } else if (settings.theme === "Light") {
+    theme.fg = g.toColor(0,0,0);
+    theme.bg = g.toColor(1,1,1);
+  }
+}
+
+loadThemeColors();
+
+Bangle.setUI({
+  mode: "clock",
+  remove: function() {
+    if (drawTimeout) clearTimeout(drawTimeout);
+    drawTimeout = undefined;
+    require("widget_utils").show();
+  },
+  redraw: draw
+});
+
+Bangle.loadWidgets();
+require("widget_utils").swipeOn();
+background.fillRect(Bangle.appRect);
+g.setColor(theme.fg).fillRect(0, h2 - 6, w, h3 + 6);
+draw();
